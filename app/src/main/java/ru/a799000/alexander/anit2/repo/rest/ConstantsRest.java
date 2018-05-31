788d347f6916fa74ab68ed926bee536965a452ac
Alexander <ag@799000.ru>
2018-06-01 09:57:05 +1000

    add tasks

--- app/src/main/java/ru/a799000/alexander/anit2/repo/rest/ConstantsRest.java
+++ app/src/main/java/ru/a799000/alexander/anit2/repo/rest/ConstantsRest.java
@@ -1,5 +1,5 @@
 package ru.a799000.alexander.anit2.repo.rest;
 
 public class ConstantsRest {
-    public static final String BASE_URL = "http://192.168.1.52/anit/hs/api/";
+    public static final String BASE_URL = "http://5.8.207.81:45249/base/hs/api/";
 }
